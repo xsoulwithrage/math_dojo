04df1be257c75b97e51b48579d43e046ce1cfedc
Ed <101188188+xsoulwithrage@users.noreply.github.com>
2023-04-10 08:58:18 -0400

    Add files via upload

---
+++ math_dojo/mathdojo.py
@@ -0,0 +1,27 @@
+class Mathdojo:
+      def __init__(self):
+            self.resultado = 0
+
+      def sumar(self, *args):
+            for arg in args:
+                  self.resultado += arg
+            return self
+      
+      def restar(self, *args):
+            for arg in args:
+                  self.resultado += arg
+            return self
+      
+md = Mathdojo()
+
+md.sumar(31, 23, 20)
+print(md.resultado)
+
+md.sumar(1).sumar(2,3).sumar(44, 56, 68)
+print(md.resultado)
+
+md.restar(23, 12, 23)
+print(md.resultado)
+
+md.restar(6).restar(3, 2).restar(3, 12, 16)
+print(md.resultado)
